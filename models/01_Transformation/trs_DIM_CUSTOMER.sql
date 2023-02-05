17f49544a4c055f9e2f79f0c1bfadcb84a6d3754
javier.vidorreta@sdggroup.com <javier.vidorreta@sdggroup.com>
2023-02-05 10:51:19 +0000

    commit trs_DIM_CUSTOMER

--- models/01_Transformation/trs_DIM_CUSTOMER.sql
+++ models/01_Transformation/trs_DIM_CUSTOMER.sql
@@ -1,3 +1,8 @@
+{{
+    config(
+        database= 'DBT_ACADEMY_PROJECT'
+    )
+}}
 with 
 
 -- ######### SOURCES ########
@@ -18,36 +23,24 @@ STG_NATION as (
 -- ######### TRANSFORMATION ########
 STG_CUSTOMERS_WO_COLUMNS as (
     select
-    C_CUSTKEY 
-    ,C_NAME 
-    ,C_ADDRESS 
-    ,C_NATIONKEY 
-    ,C_MKTSEGMENT 
-    ,C_NAME 
+    A.C_CUSTKEY AS CUSTOMER_KEY
+    ,A.C_NAME AS CUSTOMER_NAME
+    ,A.C_ADDRESS AS CUSTOMER_ADDRESS
+    ,A.C_PHONE AS CUSTOMER_PHONE
+    ,B.N_NAME AS NATION
+    ,C.R_NAME AS REGION
+    ,A.C_MKTSEGMENT AS MARKET_SEGMENT
 
-
-from STG_CUSTOMERS    
+from STG_CUSTOMERS A, STG_NATION B, STG_REGION C  
+where A.C_NATIONKEY = B.N_NATIONKEY AND B.N_REGIONKEY = C.R_REGIONKEY 
 ),
 
-
-STG_REGION_WO_COLUMNS as (
-    select
-    C_CUSTKEY as CUSTOMER_KEY
-    ,C_NAME as CUSTOMER_NAME
-    ,C_ADDRESS as CUSTOMER_ADDRESS
-    ,C_NATIONKEY as CUSTOMER_NATIONKEY
-    ,C_MKTSEGMENT as CUSTOMER_MARKETSEGMENT
-    ,C_NAME as CUSTOMER_NAME
-
-
-from STG_REGION  
-),
 
 
 
 FINAL as (
     select * 
-    ,{{ dbt_utils.surrogate_key(['']) }} as PK_DIM_CUSTOMER
+    ,{{ dbt_utils.surrogate_key(['CUSTOMER_KEY','CUSTOMER_NAME','CUSTOMER_ADDRESS']) }} as PK_DIM_CUSTOMER_TRS
     from STG_CUSTOMERS_WO_COLUMNS
 )
 
